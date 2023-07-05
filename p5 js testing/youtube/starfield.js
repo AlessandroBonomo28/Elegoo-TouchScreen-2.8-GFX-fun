a9d08d9f17edf5ecc77b9d7def99187b98bac9ea
Alexbonny28 <al.bonomo@studenti.unina.it>
2023-07-05 17:33:49 +0200

    update

---
+++ p5 js testing/youtube/starfield.js
@@ -0,0 +1,84 @@
+let stars = [];
+
+
+
+const zNear= 0.1;
+const zFar = 1000;
+
+function computeDepth(z_vertex){
+  let z = z_vertex * -(zNear + zFar)/(zNear - zFar);
+  z += (2 *zNear * zFar)/(zNear - zFar);
+  return z;
+}
+const speedStar = 0.001;
+const starCount = 200;
+const maxDistStar = 3;
+function setup() {
+  createCanvas(500, 400);
+  
+  for(let i=0;i<starCount;i++){
+    let star =[];
+    star[0] = random(-2,2);
+    star[1] = random(-2,2);
+    star[2] = random(0,maxDistStar);
+    stars.push(star);
+  }
+}
+
+
+
+function draw() {
+  background(0);
+  
+  for(let i = 0; i< starCount; i++){
+    const aspectRatio = height/width;
+    
+    
+    stars[i][2] -= deltaTime*speedStar;
+    
+    const xMov = map(mouseX,0,width,-1,1);
+    const yMov = map(mouseY,0,height,-1,1);
+    
+    let translate_x = 0;
+    let translate_y = 0;
+    let translate_z =0;
+    
+    
+    
+    stars[i][0] -= xMov * deltaTime*speedStar
+    stars[i][1] -= yMov * deltaTime*speedStar
+    if(stars[i][2]<0){
+      stars[i][0] = random(-2 + xMov*2,2 + xMov*2);
+      stars[i][1] = random(-2 + yMov*2,2 + yMov*2);
+      stars[i][2] = maxDistStar;
+    }
+      
+    
+    let x = stars[i][0] * aspectRatio + translate_x;
+    let y = stars[i][1] + translate_y * -1;
+    let z = stars[i][2] +translate_z;
+    
+    let zDepth = computeDepth(z);
+    
+    if(z!=0){ // normalizzazione -> -1,1
+      x/=z;
+      y/=z;
+      zDepth/=z;
+    }
+    
+    
+    
+    x = map(x,-1,1,0,width);
+    y = map(y,-1,1,0,height);
+    
+    if(zDepth< 1){
+      fill(255)
+      const r = map(z,0,maxDistStar, 5,0);
+      ellipse(x,y,r,r)
+    }
+    
+    
+    
+  }
+  
+}
